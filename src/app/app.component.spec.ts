80587c65bd409d7e588f5c7aae38f1369fe58c56
Mirza Medar <mirzame@maestralsolutions.com>
2020-01-31 15:17:05 +0100

    nesto 2

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,13 +19,13 @@ describe('AppComponent', () => {
   it(`should have as title 'jenkins-test'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).not.toEqual('jenkins-test');
+    expect(app.title).toEqual('jenkins-test');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).not.toContain('Welcome to jenkins-test!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to jenkins-test!');
   });
 });
