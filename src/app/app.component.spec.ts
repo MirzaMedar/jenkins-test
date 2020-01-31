3d4490d20a0f6ed0099105fb337b4a6a6fc26ddc
Mirza Medar <mirzame@maestralsolutions.com>
2020-01-31 15:18:38 +0100

    asd

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,7 +19,7 @@ describe('AppComponent', () => {
   it(`should have as title 'jenkins-test'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('jenkins-test');
+    expect(app.title).not.toEqual('jenkins-test');
   });
 
   it('should render title in a h1 tag', () => {
