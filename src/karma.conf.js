acab994ea4c09daa8f19ed12f7f227ff8938fea5
Mirza Medar <mirzame@maestralsolutions.com>
2020-02-03 08:59:40 +0100

    reporters

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -23,7 +23,7 @@ module.exports = function (config) {
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['progress', 'kjhtml', 'teamcity'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
